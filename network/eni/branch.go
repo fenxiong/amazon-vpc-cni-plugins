98cf1cfa1acd50e4471d97a2f5fd07ebc7f612a9
fenxiong <fenxiong@amazon.com>
2019-05-28 17:46:53 -0700

    fix race

--- network/eni/branch.go
+++ network/eni/branch.go
@@ -18,6 +18,7 @@ package eni
 import (
 	"fmt"
 	"net"
+	"time"
 
 	log "github.com/cihub/seelog"
 	"github.com/vishvananda/netlink"
@@ -58,6 +59,10 @@ func (branch *Branch) AttachToLink(setMACAddress bool) error {
 	la := netlink.NewLinkAttrs()
 	la.Name = branch.linkName
 	la.ParentIndex = branch.trunk.linkIndex
+	if setMACAddress && branch.macAddress != nil {
+		log.Infof("Setting branch mac address to %s", branch.macAddress)
+		la.HardwareAddr = branch.macAddress
+	}
 	vlanLink := &netlink.Vlan{LinkAttrs: la, VlanId: branch.isolationID}
 
 	log.Infof("Creating vlan link for branch [%s]: %+v", branch.linkName, vlanLink)
@@ -67,15 +72,17 @@ func (branch *Branch) AttachToLink(setMACAddress bool) error {
 	}
 
 	branch.linkIndex = vlanLink.Index
-	if setMACAddress && branch.macAddress != nil {
-		// Set VLAN link MAC address to customer branch ENI MAC address.
-		if err := netlink.LinkSetHardwareAddr(vlanLink, branch.macAddress); err != nil {
-			log.Errorf("Failed to set MAC address for branch [%s] %v: %v",
-				branch.linkName, branch.macAddress, err)
-			return err
-		}
-		return nil
-	}
+	//if setMACAddress && branch.macAddress != nil {
+	//	// Set VLAN link MAC address to customer branch ENI MAC address.
+	//	if err := netlink.LinkSetHardwareAddr(vlanLink, branch.macAddress); err != nil {
+	//		log.Errorf("Failed to set MAC address for branch [%s] %v: %v",
+	//			branch.linkName, branch.macAddress, err)
+	//		return err
+	//	}
+	//	return nil
+	//}
+	log.Infof("Sleep 20 seconds...")
+	time.Sleep(20 * time.Second)
 
 	log.Debugf("Skip setting hardware address for branch [%s] overrideMAC: %t",
 		branch.linkName, setMACAddress)
